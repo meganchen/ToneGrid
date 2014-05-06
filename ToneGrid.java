f71bc824aa34dc3e734c54d9ac5c754be27fc27c
Megan Chen <mchen0000@gmail.com>
2014-05-06 11:01:16 -0400

    First commit
    doc - documentation for JFugue class
    org - source code for JFugue class

---
+++ ToneGrid.java
@@ -0,0 +1,20 @@
+/**ToneGrid class. Contains the LinkedLists that keep track of which notes are selected in the tone grid.
+
+Author: Megan Chen
+Date Created: May 6, 2014
+**/
+
+import java.util.*;
+public class ToneGrid{
+	
+	private LinkedList<LinkedList<Boolean>> beats;
+	private LinkedList<Boolean> notes;
+	
+	public ToneGrid(){
+		beats = new LinkedList<LinkedList<Boolean>>;
+		notes = new LinkedList<Boolean>;
+	}
+	
+	public static void main(String[]args){
+	}
+}
