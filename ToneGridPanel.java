3d47f32d1635d279b91eb83fe30aa392ed102c33
Megan Chen <mchen0000@gmail.com>
2014-05-16 15:20:29 -0400

    ToneGridGUI.java

---
+++ ToneGridPanel.java
@@ -0,0 +1,19 @@
+/*ToneGridPanel.java
+	
+	Date Created: May 16, 2014
+	Creator: Megan Chen
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ToneGridPanel extends JPanel{
+
+	//Constructor
+	public ToneGridPanel(){
+		setPreferredSize(new Dimension(500,500));
+		
+		
+	}
+}
