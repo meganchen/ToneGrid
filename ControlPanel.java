2f212a0a5f65deae310ce6f369502f64d614a240
gcpx100 <gchuang@wellesley.edu>
2014-05-17 20:35:55 -0400

    updated controlpanel.java

--- ControlPanel.java
+++ ControlPanel.java
@@ -13,36 +13,64 @@ import java.util.*;
 public class ControlPanel extends JPanel{
 	private ToneGrid tg;
 	//private Hashtable<String, LinkedList<LinkedList<Boolean>>> hash;
-	private JButton clear, save, load;
+	private JButton clear, start, pause, save, load, selectMode;
 	private JTextField saveTextBox;
+	private JComboBox loadBox, modeBox;
 	
 	//constructor
 	public ControlPanel(ToneGrid t){
 		tg = t;
+
 		clear = new JButton("Clear");
+		clear.setPreferredSize(new Dimension(110, 30));
+
+		start = new JButton("Start");
+		start.setPreferredSize(new Dimension(110, 30));
+
+		pause = new JButton("Pause");
+		pause.setPreferredSize(new Dimension(110, 30));
+
 		save = new JButton("Save");
+		save.setPreferredSize(new Dimension(110, 30));
+
 		load = new JButton("Load");
+		load.setPreferredSize(new Dimension(110, 30));
+
+		selectMode = new JButton("Select mode");
+		selectMode.setPreferredSize(new Dimension(110, 30));
+
 		saveTextBox = new JTextField("Enter name");
-		setPreferredSize(new Dimension(370,100));
+		saveTextBox.setPreferredSize(new Dimension(110, 30));
+
+		setPreferredSize(new Dimension(365,120));
 		setBackground(Color.GRAY);
-			
-		add(load);
-		add(saveTextBox);
-		add(save);
-		add(clear);
+
+		String[] loads = {"Test1", "Test2"}; //dummy tester
+		loadBox = new JComboBox(loads);
+		loadBox.setPreferredSize(new Dimension(110, 30));
+
+		String[] modes = {"Mode 1", "Mode 2", "Mode 3"};
+		modeBox = new JComboBox(modes);
+		modeBox.setPreferredSize(new Dimension(110, 30));
+
+		add(start); add(pause); add(clear);
+		add(modeBox); add(saveTextBox); add(loadBox);
+		add(selectMode); add(save); add(load);
 	}
 	
 	private class ButtonListener implements ActionListener{
 		public void actionPerformed (ActionEvent event){
-			if(event.getSource() == clear)
+			if(event.getSource() == clear) {
 				tg.clear();
+			}
 			else if(event.getSource() == save){
 				String s = "" + saveTextBox.getText();
 				tg.save(s);
 			}
-			else if(event.getSource() == load)
+			else if(event.getSource() == load) {
 				tg.load("wut");
-		}
+			}
+		}	
 	}
 
 }
